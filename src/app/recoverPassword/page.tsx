d944866a0043f330a4a3cbdcab8de80c4b55367a
Christian Ivan Ledesma <cristuca1234@gmail.com>
2025-10-10 02:25:14 -0300

    feature/login - pagina-de-recupero-contraseña

---
+++ src/app/recoverPassword/page.tsx
@@ -0,0 +1,11 @@
+import RecoverPasswordForm from "../components/Form/recoverPassword"
+export default function ForgotPasswordForm() {
+
+   return( 
+    <div className="relative flex justify-center items-center min-h-screen bg-gray-50">
+
+        <RecoverPasswordForm/>
+
+    </div>
+    )
+}
