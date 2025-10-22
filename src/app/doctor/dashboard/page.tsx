be417510ccbba94cff6bec178e8e9bb903213511
Diego Mateus <fcdiegomateus@gmail.com>
2025-10-22 08:55:18 -0500

    Implementación de H1 y botón de nueva cita (check point antes de calendario)

--- src/app/doctor/dashboard/page.tsx
+++ src/app/doctor/dashboard/page.tsx
@@ -1,10 +1,24 @@
-
+import { HiMiniPlus } from "react-icons/hi2";
 
 export default function DoctorDashboardPage() {
   return (
-    <div className="mt-20">
-      <h1>Dashboard del Doctor</h1>
-      <p className="text-primary font-medium">Hola mundo</p>
+    <div className="mt-16">
+        <header className="flex flex-row items-center justify-between">
+            <div className="flex flex-col gap-2">
+                <h1 className="text-primary text-2xl font-medium">Mi Agenda Médica</h1>
+                <p className="text-base">Gestiona tus consultas y pacientes</p>
+            </div>
+            <div className="">
+                <button
+                    type="button"
+                    className="flex items-center gap-2 bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/90 transition-colors"
+                >
+                    <HiMiniPlus className="w-5 h-5"/>
+                    Nueva Cita
+                </button>
+            </div>
+        </header>
+
     </div>
   )
 }
